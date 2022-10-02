dea3076b84aefa4cca6cb1914f78017d0c041298
infinity <leobartolome97@gmail.com>
2022-10-02 22:08:19 +0800

    Feat: init setup

---
+++ src/CSVQueryCSharp/src/search.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSVQueryCSharp
+{
+    public class Search
+    {
+        public static void Main(string[] args)
+        {            
+            foreach (string arg in args)
+                Console.WriteLine(arg);
+        }
+    }
+}
---
+++ test/CSVQueryCSharp.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CSVQueryCSharp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
